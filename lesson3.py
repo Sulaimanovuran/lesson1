d7abe07ad8ba4cfca391f13254749e83929dd2ca
Sulaimanovuran <sulaimanovuran@gmail.com>
2022-06-09 18:52:32 +0600

    new repo

---
+++ lesson3.py
@@ -0,0 +1,15 @@
+'''
+GitHub - это онлайн сервис, 
+который предостовляет услуги по хранению 
+репрозиориев  и позволяет управлять ими
+
+git - распределенная система контроля версий 
+(розволяет записывать изменения, обновлять получать и тд 
+или же вернутся к старым версиям проекта)
+
+'''
+
+# git rm --cached <file> fddfdfdfd
+# token   ----   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
+
+# ;lr;elr;welrt';rltr;ertkjfd;lkmssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+Nw/EMkks+blPVFBBmCcG+PmEtm33Pd3Y9fAI6HUkS7O+sqnYSnef6KYpkVQ2f9ROLwDil1F25hjEsiglpqU1i1zKkybXaqEF20fJAx2KHvTqWnE/bSTPTvBcZhJ4XLkBW1Wo26bFl5OcyGMOSStulaZAEdsisM8XfrFb7QpiJhouOcV/n661rbraX+UlR2RboaXwoS0wI3KDJWGgEDP9t0aQq7LDnNPhtHdExwqWjMVN/0iN2OEphzq35Tjm0Bt19f3BUMcfLJ+xZUqzlkZ0dn5DxaOe+KUAa0IU/uamwC0N0HzF+QUqNjTVodOw68hBHk96PV8fZRmqD+Ycn+tjZ7j6EbjAurqu2zb03lQVwTC930X4r3H5xWmiUCZ+orLO5EghAqLF8evWi/Yp8PC2PGrzOZGVCtGWd1kM5rMrzBhJIPi3GyF9uMtf3xaKgPaQllXDIYDJrPvwDiPotvG45AGEn0b739KiPCzGrNXfSA4pY727cQurBm7TCWc2sdD9ZrdtyC/IOeqDNe17etudsFpdt15QY++CwbWi76JHD8dQaojS6oOKglb4gK2p9Z2RWvHlSb/s5tmz7bJXLIPtyAOmR83mH3OWY21DT1Qbs/BMSNe7AYuBxPRwmA0Ow8v7SnSPi1/mfnCFhsmjc0yvQkMmSiF3+4tx/qfSkrOBmw== Sulaimanovuran
